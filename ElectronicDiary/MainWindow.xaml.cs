639bc1df8d3d8032c9957a29183c543bd1142869
Ivan <sirius9764@gmail.com>
2015-05-31 16:42:28 +0300

    Class MainWindow splited up

--- ElectronicDiary/MainWindow.xaml.cs
+++ ElectronicDiary/MainWindow.xaml.cs
@@ -25,52 +25,13 @@ namespace ElectronicDiary
         {
             InitializeComponent();
             model=new ElectronicDiaryDBEntities();
-            Admin.Visibility = Visibility.Hidden;
+            Admin.Visibility = Visibility.Visible;
             Client.Visibility=Visibility.Hidden;
+            Logining.Visibility = Visibility.Hidden;
+            
         }
 
-        enum role
-        {
-            administrator,student,teacher
-        }
-        private void Entering(object sender, RoutedEventArgs e)
-        {
-            if (Login.Text.Length == 0 || Password.Password.Length == 0)
-            {
-                MessageBox.Show("Логин или пароль не были введены");
-                return;
-            }
-
-            var z = ((from asd in model.Users
-                      where asd.Login == Login.Text
-                      where asd.Password == Password.Password
-                      select asd));
-            if (z.LongCount() > 0)
-            {
-                if (z.First().Role == role.administrator.ToString())
-                {
-                    Logining.Visibility = Visibility.Hidden;
-                    Admin.Visibility = Visibility.Visible;
-                }
-                else if (z.First().Role == role.student.ToString())
-                {
-                    Logining.Visibility = Visibility.Hidden;
-                    Student.Visibility = Visibility.Visible;
-
-
-                }
-                else
-                    if (z.First().Role == role.teacher.ToString())
-                {
-                    Logining.Visibility = Visibility.Hidden;
-                    Teacher.Visibility = Visibility.Visible;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Логин или пароль были введены некоректно");
-            }
-        }
+       
 
 
     }
---
+++ ElectronicDiary/Source/LoginPanel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ElectronicDiary
+{
+    partial class MainWindow : Window
+    {
+        enum role
+        {
+            administrator, student, teacher
+        }
+        private void Entering(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text.Length == 0 || Password.Password.Length == 0)
+            {
+                MessageBox.Show("Логин или пароль не были введены");
+                return;
+            }
+
+            var z = ((from asd in model.Users
+                      where asd.Login == Login.Text
+                      where asd.Password == Password.Password
+                      select asd));
+            if (z.LongCount() > 0)
+            {
+                if (z.First().Role == role.administrator.ToString())
+                {
+                    Logining.Visibility = Visibility.Hidden;
+                    Admin.Visibility = Visibility.Visible;
+                }
+                else if (z.First().Role == role.student.ToString())
+                {
+                    Logining.Visibility = Visibility.Hidden;
+                    Student.Visibility = Visibility.Visible;
+
+
+                }
+                else
+                    if (z.First().Role == role.teacher.ToString())
+                    {
+                        Logining.Visibility = Visibility.Hidden;
+                        Teacher.Visibility = Visibility.Visible;
+                    }
+            }
+            else
+            {
+                MessageBox.Show("Логин или пароль были введены некоректно");
+            }
+        }
+    }
+}
