ca81406b705fc989c07c1bb84a31e611b7ad8ca4
datrax <datrax@mail.ru>
2015-05-31 15:59:30 +0300

    logining system

--- ElectronicDiary/MainWindow.xaml.cs
+++ ElectronicDiary/MainWindow.xaml.cs
@@ -20,9 +20,56 @@ namespace ElectronicDiary
     /// </summary>
     public partial class MainWindow : Window
     {
+        ElectronicDiaryDBEntities model;
         public MainWindow()
         {
             InitializeComponent();
+            model=new ElectronicDiaryDBEntities();
+            Admin.Visibility = Visibility.Hidden;
+            Client.Visibility=Visibility.Hidden;
+        }
+
+        enum role
+        {
+            administrator,student,teacher
+        }
+        private void Entering(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text.Length == 0 || Password.Password.Length == 0)
+            {
+                MessageBox.Show("Логин или пароль не были введены");
+                return;
+            }
+
+            var z = ((from asd in model.Users
+                      where asd.Login == Login.Text
+                      where asd.Password == Password.Password
+                      select asd));
+            if (z.LongCount() > 0)
+            {
+                if (z.First().Role == role.administrator.ToString())
+                {
+                    Logining.Visibility = Visibility.Hidden;
+                    Admin.Visibility = Visibility.Visible;
+                }
+                else if (z.First().Role == role.student.ToString())
+                {
+                    Logining.Visibility = Visibility.Hidden;
+                    Student.Visibility = Visibility.Visible;
+
+
+                }
+                else
+                    if (z.First().Role == role.teacher.ToString())
+                {
+                    Logining.Visibility = Visibility.Hidden;
+                    Teacher.Visibility = Visibility.Visible;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Логин или пароль были введены некоректно");
+            }
         }
 
 
