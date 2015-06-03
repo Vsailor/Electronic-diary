5ee73cc68d91aa3c6374ad415c95db7e84fc8447
datrax <datrax@mail.ru>
2015-06-03 20:28:55 +0300

    Subject remove fixed styles added miscellaneous bugs fixed

--- ElectronicDiary/Source/AdminSubjects.cs
+++ ElectronicDiary/Source/AdminSubjects.cs
@@ -44,11 +44,15 @@ namespace ElectronicDiary
                            where
                                subjects.Name == toRemove
                            select subjects).FirstOrDefault();
+            
             if (subject == null)
             {
                 StatusBar.Content = "Subject not found";
                 return;
             }
+            model.Subjects_Teachers.RemoveRange(subject.Subjects_Teachers);
+            model.Schedules.RemoveRange(subject.Schedules);
+            model.Marks.RemoveRange(subject.Marks);
             model.Subjects.Remove(subject);
             try
             {
